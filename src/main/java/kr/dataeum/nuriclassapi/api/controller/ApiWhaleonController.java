563ea65bb39e1b385fc68333313fd4710f6fe357
separk <deseeun@dataeum.kr>
2023-07-11 20:13:14 +0900

    초기 세팅

--- src/main/java/kr/dataeum/nuriclassapi/api/controller/ApiWhaleonController.java
+++ src/main/java/kr/dataeum/nuriclassapi/api/controller/ApiWhaleonController.java
@@ -1,7 +1,13 @@
 package kr.dataeum.nuriclassapi.api.controller;
 
 import kr.dataeum.nuriclassapi.common.controller.CommonDefaultController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/whaleon")
 public class ApiWhaleonController extends CommonDefaultController {
 
 
