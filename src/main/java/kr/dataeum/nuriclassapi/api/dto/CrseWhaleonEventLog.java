6b14a03bfcf8247f0368f9922f20cd66c69b3597
separk <deseeun@dataeum.kr>
2023-08-03 11:32:55 +0900

    작업중

---
+++ src/main/java/kr/dataeum/nuriclassapi/api/dto/CrseWhaleonEventLog.java
@@ -0,0 +1,16 @@
+package kr.dataeum.nuriclassapi.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CrseWhaleonEventLog {
+    private String partcptInfoId;//참가정보Id
+    private String progrmId;//프로그램ID
+    private String adhrncUserId;//유저ID
+    private String adhrncUserNm;//유저명
+    private String partcptDe;//참가일
+    private String partcptTime;//참가시간
+    private String endTime;//종료시간
+}
---
+++ src/main/java/kr/dataeum/nuriclassapi/api/dto/RltmLctrPartcptInfo.java
@@ -0,0 +1,18 @@
+package kr.dataeum.nuriclassapi.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RltmLctrPartcptInfo {
+    private String partcptInfoId;//참가정보Id
+    private String lctrId;//강연Id
+    private String uprismId;//유프리즘ID
+    private String adhrncUserId;//유저ID
+    private String adhrncUserNm;//유저명
+    private String partcptDe;//참가일
+    private String partcptTime;//참가시간
+    private String endTime;//종료시간
+    private String progTime;//머문시간
+}
